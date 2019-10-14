123029a945366a3bfa90a1157cf8cab4bd04a219
guofanguofan123 <53296994+guofanguofan123@users.noreply.github.com>
2019-10-14 21:40:40 +0800

    Add files via upload

---
+++ cindex.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="cindex-container">
+    <h1>豆瓣最受欢迎的影评</h1>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style scoped>
+  .cindex-container{
+    width:820px;
+    position: fixed;
+    justify-content: space-between;
+    }
+  .cindex-container>h1{color:#494949;}
+</style>
---
+++ subject.vue
@@ -0,0 +1,96 @@
+<template>
+    <div>
+        <!-- top 250 begin -->
+        <div class="top205">
+            <span class="top250-no">No.205</span>
+            <span class="top250-link">
+                <a href="https://movie.douban.com/top250">
+                    豆瓣电影Top250
+                </a>
+            </span>
+        </div>
+        <!-- top 250 end -->
+        <h1>
+            <span>初恋这件小事 สิ่งเล็กเล็กที่เรียกว่า...รัก</span>
+            <span>(2010)</span>
+        </h1>
+        <div class="article">
+            <div class="indent clearfix">
+                <div class="subjectwrap clearfix">
+                    <div class="mainpic">
+                        <a href="https://movie.douban.com/subject/4739952/photos?type=R">
+                            <img src="https://img3.doubanio.com/view/photo/s_ratio_poster/public/p1505312273.webp" alt="">
+                        </a>
+                    </div>
+                    <div class="info">
+                        <span>
+                            <span>导演：</span>
+                            <span>
+                                <a href="https://movie.douban.com/celebrity/1313103/">
+                                    普罗萨卡·那·萨克那卡林 
+                                </a>/
+                                <a href="https://movie.douban.com/celebrity/1313104/">华森·波克彭</a>
+                            </span>
+                        </span>
+                        <span>
+                            <span>编剧：</span>
+                            <span>
+                                <a href="https://movie.douban.com/celebrity/1313103/">
+                                    普罗萨卡·那·萨克那卡林 
+                                </a>/
+                            </span>
+                        </span>
+                        <span>
+                            <span>主演：</span>
+                            <span>
+                                <a href="https://movie.douban.com/celebrity/1275794/">
+                                     平采娜·乐维瑟派布恩  
+                                </a>/
+                                <a href="https://movie.douban.com/celebrity/1041011/"> 马里奥·毛瑞尔 </a>/
+                                <a href="https://movie.douban.com/celebrity/1313263/"> 苏达拉·布查 </a>
+                            </span>
+                        </span>
+                        <span>
+                            <span>类型：</span>
+                            <span>剧情/</span>
+                            <span>喜剧/</span>
+                            <span>爱情</span>
+                        </span>
+                        <span>
+                            <span>制片国家/地区：</span>
+                            <span>泰国</span>
+                        </span>
+                        <span>
+                            <span>语言：</span>
+                            <span>泰国/印度尼西亚语/英语</span>
+                        </span>
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+
+.top250-no{
+    text-align: center;
+    width: 51px;
+    height: 18px;
+    line-height: 18px;
+    color: #744900;
+    background: #ff0;
+}
+.top250-link a{
+    text-decoration: none;
+    color: #744900;
+    background:orange;
+}
+</style>
+
